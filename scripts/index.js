784199536c3a64e0237807de16f1209b97b7f2a9
Natalia Veremeenko <nveco@yandex.ru>
2022-04-04 16:33:01 +0300

    feat: настроить js для сохранения изменений в профиле

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,30 @@ profileEditButton.addEventListener('click', popupOpen);
 modalCloseBtn.addEventListener('click', function () {
     modalWindow.classList.remove('popup_opened');
 })
+
+
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__bio');
+let nameChange = document.querySelector('.Profile__name');
+let bioChange = document.querySelector('.Profile__bio');
+let modalSaveBtn = document.querySelector('.popup__button-save')
+
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    nameChange.textContent = nameInput.value;
+    bioChange.textContent = jobInput.value;
+  
+ }
+
+formElement.addEventListener('submit', formSubmitHandler);
+modalSaveBtn.addEventListener('click', formSubmitHandler);
+modalSaveBtn.addEventListener('click', function () {
+    modalWindow.classList.remove('popup_opened');
+});
+
+
+
