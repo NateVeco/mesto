07af9133a8b1cf88655a7e128a450b6acc97dd0a
Natalia Veremeenko <nveco@yandex.ru>
2022-04-11 12:13:25 +0300

    fix: исправить скрипт для формы редактирования

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,40 @@
-// Открытие попапа редактирования профиля
-
 const profileEditButton = document.querySelector('.profile__edit-button');
-
 const modalWindow = document.querySelector('.popup');
-
 const modalCloseBtn = document.querySelector('.popup__button-close');
-
-function popupOpen() {
-    modalWindow.classList.add('popup_opened');
-}
-
-profileEditButton.addEventListener('click', popupOpen);
-
-
-modalCloseBtn.addEventListener('click', function () {
-    modalWindow.classList.remove('popup_opened');
-})
-
-// Форма редактирование профиля и закрытия попапа
+const modalSaveBtn = document.querySelector('.popup__button-save');
 
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__text_type_name');
 let jobInput = document.querySelector('.popup__text_type_bio');
 let nameChange = document.querySelector('.profile__name');
 let bioChange = document.querySelector('.profile__bio');
-let modalSaveBtn = document.querySelector('.popup__button-save')
 
+// Открытие попапа редактирования профиля
+
+function popupOpen(value) {
+    modalWindow.classList.add('popup_opened');
+}
+
+// Форма редактирования профиля
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
     nameChange.textContent = nameInput.value;
     bioChange.textContent = jobInput.value;
-  
- }
+    modalWindow.classList.remove('popup_opened');
+}
+
+// Закрытие попапа редактирования профиля
+
+function popupClose() {
+    modalWindow.classList.remove('popup_opened')
+};
 
 formElement.addEventListener('submit', formSubmitHandler);
-modalSaveBtn.addEventListener('click', formSubmitHandler);
-modalSaveBtn.addEventListener('click', function () {
-    modalWindow.classList.remove('popup_opened');
-});
+modalSaveBtn.addEventListener('submit', formSubmitHandler);
+profileEditButton.addEventListener('click', popupOpen);
+modalCloseBtn.addEventListener('click', popupClose);
 
 // Форма добавления карточки
 
