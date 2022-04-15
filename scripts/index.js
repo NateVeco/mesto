a4feaeae082aae0920ab4189a2abbc0c58d46042
Natalia Veremeenko <nveco@yandex.ru>
2022-04-15 16:33:02 +0300

    feat: настроить template

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
 const initialImages = [{
-        name: 'Норвегия',
-        link: 'https://images.unsplash.com/photo-1544009520-e2ea9189f15e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'
+        name: 'Зима в Финляндии',
+        link: 'https://images.unsplash.com/photo-1584380029877-e8d0fe39d534?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
     },
     {
-        name: 'Горы Норвегии',
-        link: 'https://images.unsplash.com/photo-1508592931388-95bc7b61033d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        name: 'Финляндия',
+        link: 'https://images.unsplash.com/photo-1585727384562-8c1685105af5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+
     },
     {
+
         name: 'Аляска',
         link: 'https://images.unsplash.com/photo-1574788901656-6a9ee34a3fa7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+
     },
     {
         name: 'Дорога Аляски',
         link: 'https://images.unsplash.com/photo-1574866412308-32d9023633dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80'
     },
     {
-        name: 'Финляндия',
-        link: 'https://images.unsplash.com/photo-1585727384562-8c1685105af5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        name: 'Горы Норвегии',
+        link: 'https://images.unsplash.com/photo-1508592931388-95bc7b61033d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+
     },
     {
-        name: 'Приключение в Финляндии',
-        link: 'https://images.unsplash.com/photo-1584380029877-e8d0fe39d534?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        name: 'Норвегия',
+        link: 'https://images.unsplash.com/photo-1544009520-e2ea9189f15e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'
+
     }
 ];
-
 
 const profileEditButton = document.querySelector('.profile__edit-button');
 const modalWindow = document.querySelector('.popup');
@@ -34,15 +38,16 @@ const nameInput = document.querySelector('.popup__text_type_name');
 const jobInput = document.querySelector('.popup__text_type_bio');
 const nameChange = document.querySelector('.profile__name');
 const bioChange = document.querySelector('.profile__bio');
-
 const profileAddButton = document.querySelector('.profile__add-button');
 const profilePopupAdd = document.querySelector('.popup_type-add-images');
 const profileAddCloseBtn = profilePopupAdd.querySelector('.popup__button-close');
-
 const nameElementInput = profilePopupAdd.querySelector('.popup__text_type_name-element');
 const linkElementInput = profilePopupAdd.querySelector('.popup__text_type_link-element');
+const profileAddSaveBtn = document.querySelector('.popup__button-save');
+const elementsTable = document.querySelector('.elements__table');
+const template = document.querySelector('.template');
 
-// Открытие попапов
+
 
 function popupOpen(value) {
     modalWindow.classList.add('popup_opened');
@@ -54,8 +59,6 @@ function popupAddOpen() {
     profilePopupAdd.classList.add('popup_opened');
 }
 
-// Редактирования профиля
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
@@ -64,15 +67,53 @@ function formSubmitHandler(evt) {
     modalWindow.classList.remove('popup_opened');
 }
 
-// Добавление новой карточки - после просмотра вебинара - делать через template
+// Добавление новой карточки
 
-// function formSubmitHandler(evt) {
-//     evt.preventDefault();
+// Эта функция, чтобы получить из темпал в верстку картинки из массива
 
-//     nameChange.textContent = nameElementInput.value;
-//     bioChange.textContent = linkElementInput.value;
-//     profileAddButton.classList.remove('popup_opened');
-// }
+function render() {
+    const html = initialImages.map(getElement);
+    elementsTable.append(...html);
+}
+
+// Эта функция для получения нового элемента картинки из темплат и настроенная кнопку удалить - здесь потом сделать кнопку лайк
+
+function getElement(item) {
+    const newImage = template.content.cloneNode(true);
+    const name = newImage.querySelector(".element__title");
+    const image = newImage.querySelector('.element__image');
+    const removeButton = newImage.querySelector(".trash-button");
+
+    name.textContent = item.name;
+    image.src = item.link;
+
+    removeButton.addEventListener("click", handleRemoveElement);
+
+    return newImage;
+}
+
+function handleRemoveElement(evt) {
+    const element = evt.target.closest(".element");
+    element.remove();
+}
+
+// Функция которая добавляет новый элемент - недоделанная
+
+function handleAddElement() {
+    const element = getElement({
+        name: nameElementInput.value,
+        link: linkElementInput.value
+    });
+
+    elementsTable.prepend(element);
+    nameElementInput.value = "";
+    linkElementInput.value = "";
+}
+
+
+profileAddSaveBtn.addEventListener("click", handleAddElement);
+
+render();
 
 // Закрытие попапов 
 
@@ -91,13 +132,5 @@ modalCloseBtn.addEventListener('click', popupClose);
 profileAddCloseBtn.addEventListener('click', popupAddClose);
 
 
-// В пятницу доделать Добавление карточки и настроить лайки
-// в сб - вс остальное доделать
-
-// Лайк карточки
-
-// Удаление карточки
-
-// Открытие попапа с картинкой
-
-// Плавное открытие и закрытие попапов
+// в сб доделать Добавление карточки и настроить лайки и поправить удаление картинок чтобы сдвигалось на пустое место
+// в вск остается открытие попапа с картинкой и настроить плавное открытие и закрытие попапов
