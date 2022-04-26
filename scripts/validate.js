5b25d0592567e92a1ee8f6aa70243c6c41f4003a
Natalia Veremeenko <nveco@yandex.ru>
2022-04-26 16:29:13 +0300

    feat: добавить текст об ошибке

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,6 +1,7 @@
 function enableValidation(formSelector) {
     const profileForm = document.querySelector(formSelector);
     profileForm.addEventListener('submit', (evt) => handleProfileFormSubmit(evt));
+    profileForm.addEventListener('input', (evt) => handleProfileFormInput(evt));
 
 }
 
@@ -9,8 +10,22 @@ function handleProfileFormSubmit(evt) {
 
     const profileForm = evt.currentTarget;
     console.log(profileForm.checkValidity());
+    if (profileForm.checkValidity()) {
+        handleProfileEditFormSubmit(evt);
+    } else {
+        alert('фейл');
+    }
+}
 
+function handleProfileFormInput(evt) {
+    const profileInput = evt.target;
+    const error = document.querySelector('#${profileInput.id}-error');
+
+    console.log(error);
 
 }
+
+
+
 
 enableValidation('.popup__container');
